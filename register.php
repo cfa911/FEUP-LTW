c02e7676476262ae41509fff8107b62fb3551f25
Christopher Abreu <cfa911@outlook.pt>
2018-12-11 19:26:20 +0000

    pEGA REGISTER A DAR

--- register.php
+++ register.php
@@ -1,12 +1,14 @@
 <?php
 $dbh = new PDO('sqlite:database.db');
 
-$username = $_GET['username'];
-$email = $_GET['email'];
+$username = $_POST['username'];
+$email = $_POST['email'];
 $password = hash('sha256', 'password');
 
+/*
 $sql = file_get_contents('LTW-SQL.sql');
 $qr = $dbh->exec($sql);
+clear TABLES*/
 
 $stmt = $dbh->prepare('INSERT INTO UTILAISER (username, password, email)
                        VALUES (:username, :password, :email)');
@@ -20,7 +22,7 @@ $get = $dbh->prepare('SELECT * FROM UTILAISER');
 $get->execute();
 $picada = $get->fetchAll();
 foreach($picada as $picada){
-    echo $picada['password'];
+    echo $picada['username'];
 }
 
 ?>
--- registerscript.js
+++ registerscript.js
@@ -1,24 +1,11 @@
 
 let register = document.getElementsByTagName('section')[0];
 let password_box = document.getElementsByTagName('input')[2];
-
+document.getElementsByTagName('input')[3].disabled = true;
 password_box.addEventListener('input', function () {
   test_strength();
   event.preventDefault();
 });
-
-register.addEventListener('submit', function () {
-
-
-    let username = document.getElementsByTagName('input')[0].value;
-    let e_mail = document.getElementsByTagName('input')[1].value;
-    let password = document.getElementsByTagName('input')[2].value;
-
-    if((username != '') && (e_mail != '') && (password != '') && (document.querySelector('#Strength').textContent != 'Very Weak'  && document.querySelector('#Strength').textContent != 'Weak')){
-    }
-
-    event.preventDefault();
-  });
 
 
 
@@ -45,18 +32,22 @@ function test_strength(){
       password_strenght --;
     }
 
+
     switch(password_strenght){
       case 3:
         document.querySelector('#Strength').style = "color:green";
         document.querySelector('#Strength').textContent = 'Very Strong';
+        document.getElementsByTagName('input')[3].disabled = false;
         break;
       case 2:
         document.querySelector('#Strength').style = "color:green";
         document.querySelector('#Strength').textContent = 'Strong';
+        document.getElementsByTagName('input')[3].disabled = false;
         break;
       case 1:
         document.querySelector('#Strength').style = "color:yellow";
         document.querySelector('#Strength').textContent = 'Good';
+        document.getElementsByTagName('input')[3].disabled = false;
         break;
       case 0:
       case -1:
@@ -64,10 +55,13 @@ function test_strength(){
       case -3:
         document.querySelector('#Strength').style = "color:red";
         document.querySelector('#Strength').textContent = 'Weak';
+        document.getElementsByTagName('input')[3].disabled = true;
         break;
       default:
         document.querySelector('#Strength').style = "color:red";
         document.querySelector('#Strength').textContent = 'Very Weak';
+        document.getElementsByTagName('input')[3].disabled = true;
+
         break;
     }
 
