e46f991ebf0caf979daf49a00e89f67791771dce
Tiago Cardoso <up201605762@fe.up.pt>
2018-12-18 00:41:14 +0000

    falta fazer o css do prof

---
+++ profile.php
@@ -0,0 +1,75 @@
+<?php
+  include('login.php');
+  $dbh = new PDO('sqlite:database.db');
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Socially</title>
+    <meta charset="UTF-8">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/layout.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <!--<link href="forms.css" rel="stylesheet">-->
+</head>
+
+<body>
+    <header>
+            <h1>
+                <a href="index.html">
+                    <img id="logo" src="Logo.png" >
+                </a>
+                <a href="index.html">Socially</a>
+            </h1>
+            <h2> Where life friendships are made</h2>
+    </header>
+
+    <nav id="menu">
+        <ul>
+            <li><a href="login.html">Login</a></li>
+            <li><a href="register.html">Register</a></li>
+        </ul>
+    </nav>
+
+    <div id="title">
+        <img id="logo" src="Logo.png">
+        <h1>Socially</h1>
+    </div>
+
+    <div id="center">
+        <h1>Connect with your friends and family</h1>
+        <img id="photo" src=<?php
+        $stmt = $dbh->prepare('select file_name from IMAGES where imageID = ( select imageID from Profile where username = ?)');
+        $stmt->execute(array($_SESSION['username']));
+        $result = $stmt->fetch();
+        echo $result['file_name'];?>>
+        <p><?php
+        $stmt = $dbh->prepare('select firstName from Profile where username = ?');
+        $stmt->execute(array($_SESSION['username']));
+        $result = $stmt->fetch();
+        echo $result['firstName'];?></p>
+        <p><?php
+        $stmt = $dbh->prepare('select lastName from Profile where username = ?');
+        $stmt->execute(array($_SESSION['username']));
+        $result = $stmt->fetch();
+        echo $result['lastName'];?></p>
+        <p><?php
+        $stmt = $dbh->prepare('select age from Profile where username = ?');
+        $stmt->execute(array($_SESSION['username']));
+        $result = $stmt->fetch();
+        echo $result['age'];?></p>
+        <p><?php
+        $stmt = $dbh->prepare('select karma from Profile where username = ?');
+        $stmt->execute(array($_SESSION['username']));
+        $result = $stmt->fetch();
+        echo $result['karma'];?></p>
+    </div>
+
+    <footer>
+        <p>&copy; Socially, 2018</p>
+    </footer>
+
+</body>
+
+</html>
